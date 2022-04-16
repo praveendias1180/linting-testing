3aa0a43c909217e8c521103bdf03c638bbd8ca69
Praveen Dias <praveendias1180@gmail.com>
2022-04-16 22:48:05 +0530

    Eslint line comments

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const test = 'I am a test';
 console.error(`Test: ${test}`)
 const test2 = 'Another one.';
+
+/* eslint-disable no-alert, no-console */
+
+alert('foo');
+console.log('bar');
+
+/* eslint-enable no-alert, no-console */
